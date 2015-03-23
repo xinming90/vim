ad72c3238c24c3726ddde5b88463716b8cb7df75
xinming90 <panxinming90@gmail.com>
2015-03-23 20:08:00 +0800

    update

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+mkdir -p ~/.vim/backup
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 curl https://raw.githubusercontent.com/xinming90/vim/master/vimrc -o ~/.vimrc
 vim +PluginInstall
